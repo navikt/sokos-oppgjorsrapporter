9b74bbe2d0b066ab0a91cdc397fcc12396b63f3f
Harald Meland <harald.roisland.meland@nav.no>
2025-09-16 22:36:16 +0200

    TOB-5190: Skru av withReuse i database-testcontaineren når testene kjøres fra GitHub Actions

--- src/test/kotlin/no/nav/sokos/oppgjorsrapporter/TestContainer.kt
+++ src/test/kotlin/no/nav/sokos/oppgjorsrapporter/TestContainer.kt
@@ -9,18 +9,20 @@ object TestContainer {
     val postgres: PostgreSQLContainer<Nothing> by lazy {
         PostgreSQLContainer<Nothing>(DockerImageName.parse(postgresImage)).apply {
             // Med 'reuse' påskrudd forblir containeren med databasen levende etter at testene har kjørt.
-            // Dermed kan man gjøre ting som:
+            // Dette kan være nyttig når man kjører testene lokalt, siden man da kan gjøre ting som:
             //  * peke IDEen sin på databasen (slik at den kan hente ned skjemaet og hjelpe med skriving av spørringer/migreringer),
             //  * bruke f.eks. psql til å se på hvordan dataene ser ut etter at en test har kjørt,
             //  * etc.
             //
             // En liten ulempe er dog at hvis man har endret på en migrering etter at testene har kjørt, vil flyway feile ved neste
-            // kjøring feile (fordi sjekksummen på migreringen er endret).
+            // kjøring (fordi sjekksummen på migreringen er endret).
             // Såfremt det er en ny, ikke-deployet migrering det er snakk om, kan man enkelt blidgjøre flyway ved å manuelt kverke
             // postgres-containeren (i Podman Desktop: Gå til "Containers", trykk på søppelkasse-ikonet til høyre på linja med
             // postgres-containeren) før man kjører testene igjen.  En ny container vil da bli laget - med endret lytte-port, så
-            // man må antakelig gjøre en liten konfig-endring i IDE før database-skjema-integrasjonen virker igjen.
-            withReuse(true)
+            // man må antakelig gjøre en liten konfig-endring i IDE før database-skjema-integrasjonen der virker igjen.
+            //
+            // Hvis testene kjøres fra GitHub Actions, skal database-containeren aldri forbli levende.
+            withReuse(System.getenv("GITHUB_ACTIONS") == null)
             withUsername("test-admin")
             waitingFor(Wait.defaultWaitStrategy())
             start()
