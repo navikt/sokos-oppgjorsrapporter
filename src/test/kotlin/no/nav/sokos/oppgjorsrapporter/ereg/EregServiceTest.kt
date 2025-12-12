62f09a7b0fa4b32fb4362f50a679b6c51bc34d52
Harald Meland <harald.roisland.meland@nav.no>
2025-12-12 01:10:50 +0100

    TOB-5621: Cleanup i EregServiceTest - ingen grunn til å måtte bruke spyk() der.

--- src/test/kotlin/no/nav/sokos/oppgjorsrapporter/ereg/EregServiceTest.kt
+++ src/test/kotlin/no/nav/sokos/oppgjorsrapporter/ereg/EregServiceTest.kt
@@ -10,24 +10,18 @@ import io.ktor.http.headersOf
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.utils.io.ByteReadChannel
 import io.mockk.mockk
-import io.mockk.spyk
 import java.net.URI
 import kotlinx.coroutines.test.runTest
 import no.nav.sokos.oppgjorsrapporter.config.commonJsonConfig
 import no.nav.sokos.oppgjorsrapporter.metrics.Metrics
 import no.nav.sokos.oppgjorsrapporter.utils.eregResponse
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class EregServiceTest {
-
     @Test
-    fun `hentOrganisasjonsNavnOgAdresse bør returnere en korekt formatert organisasjon navn og addresse`() = runTest {
-        val mockMetrics = mockk<Metrics>(relaxed = true)
-
-        // Arrange
-        val orgnr = "990983666"
+    fun `hentOrganisasjonsNavnOgAdresse bør returnere organisasjonens navn og adresse med riktig format`() = runTest {
         val mockEngine = MockEngine {
             respond(
                 content = ByteReadChannel(eregResponse),
@@ -35,27 +29,22 @@ class EregServiceTest {
                 headers = headersOf(HttpHeaders.ContentType, "application/json"),
             )
         }
-
         val mockHttpClient = HttpClient(mockEngine) { install(ContentNegotiation) { json(commonJsonConfig) } }
-        val eregUrl = URI("http://dummy-ereg-url")
-        val eregService = spyk(EregService(eregUrl, mockHttpClient, mockMetrics), recordPrivateCalls = true)
-        val actual = eregService.hentOrganisasjonsNavnOgAdresse(orgnr)
-        val expected =
-            OrganisasjonsNavnOgAdresse(
-                organisasjonsnummer = "990983666",
-                navn = "NAV FAMILIE- OG PENSJONSYTELSER OSL",
-                adresse = "Sannergata 2, 0557 Oslo",
-            )
 
-        // Assert
-        assertEquals(expected, actual)
+        val eregService = EregService(URI("http://dummy-ereg-url"), mockHttpClient, mockk(relaxed = true))
+
+        assertThat(eregService.hentOrganisasjonsNavnOgAdresse("990983666"))
+            .isEqualTo(
+                OrganisasjonsNavnOgAdresse(
+                    organisasjonsnummer = "990983666",
+                    navn = "NAV FAMILIE- OG PENSJONSYTELSER OSL",
+                    adresse = "Sannergata 2, 0557 Oslo",
+                )
+            )
     }
 
     @Test
     fun `hentOrganisasjonsNavnOgAdresse bør kaste RuntimeException feil respons fra Ereg tjenesten`() = runTest {
-        val mockMetrics = mockk<Metrics>(relaxed = true)
-        // Arrange
-        val orgnr = "990983666"
         val mockEngine = MockEngine {
             respond(
                 content = ByteReadChannel("""{"melding": "Noe gikk galt"}"""),
@@ -64,8 +53,9 @@ class EregServiceTest {
             )
         }
         val mockHttpClient = HttpClient(mockEngine) { install(ContentNegotiation) { json(commonJsonConfig) } }
-        val eregUrl = URI("http://dummy-ereg-url")
-        val eregService = spyk(EregService(eregUrl, mockHttpClient, mockMetrics), recordPrivateCalls = true)
-        assertThrows<RuntimeException> { eregService.hentOrganisasjonsNavnOgAdresse(orgnr) }
+
+        val eregService = EregService(URI("http://dummy-ereg-url"), mockHttpClient, mockk<Metrics>(relaxed = true))
+
+        assertThrows<RuntimeException> { eregService.hentOrganisasjonsNavnOgAdresse("990983666") }
     }
 }
