f7f694e16ffed8c6bca137d5ce0c3e434671a331
Harald Meland <harald.roisland.meland@nav.no>
2025-12-19 02:38:36 +0100

    TOB-5812: Forbedringen ble feil; forsøkt rettet nå

--- src/main/kotlin/no/nav/sokos/oppgjorsrapporter/rapport/RapportRepository.kt
+++ src/main/kotlin/no/nav/sokos/oppgjorsrapporter/rapport/RapportRepository.kt
@@ -351,23 +351,25 @@ class RapportRepository(private val clock: Clock) {
                 WITH rtyp AS (SELECT * FROM unnest(enum_range(null::rapport.rapport_type)) AS rapport_type),
                      dim0 AS (SELECT *
                               FROM rtyp,
-                              (VALUES ('0')) AS t1(antall_nedlastinger),
-                              (VALUES ('n/a')) AS t2(auth_type)),
+                                   (VALUES ('0')) AS t1(antall_nedlastinger),
+                                   (VALUES ('n/a')) AS t2(auth_type)),
                      dimensjoner AS (SELECT *
                                      FROM rtyp,
                                           (VALUES ('1'), ('2+')) AS t1(antall_nedlastinger),
                                           (VALUES ('entraid'), ('systembruker')) AS t2(auth_type)
                                      UNION
-                                     SELECT * FROM dim0),
+                                     SELECT *
+                                     FROM dim0),
                      telling AS (SELECT r.id    AS rapport_id,
                                         r.type  AS rapport_type,
-                                        CASE WHEN COUNT(a.id) IS NULL THEN 'n/a'
-                                             WHEN COUNT(a.id) <= 1 THEN COUNT(a.id)::text
-                                             ELSE '2+'
-                                             END AS antall_nedlastinger,
                                         CASE
+                                            WHEN COUNT(a.id) <= 1 THEN COUNT(a.id)::text
+                                            ELSE '2+'
+                                            END AS antall_nedlastinger,
+                                        CASE
+                                            WHEN brukernavn IS NULL THEN 'n/a'
                                             WHEN brukernavn LIKE 'entraid:%'
-                                              OR brukernavn LIKE 'azure:%' THEN 'entraid'
+                                                OR brukernavn LIKE 'azure:%' THEN 'entraid'
                                             ELSE 'systembruker'
                                             END AS auth_type
                                  FROM rapport.rapport r
