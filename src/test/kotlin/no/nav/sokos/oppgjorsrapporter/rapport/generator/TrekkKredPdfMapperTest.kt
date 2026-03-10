b0d44660d401d3a62e3fdd956758c53fbc1e762e
Magnar Brandsdal <magnar.brandsdal@nav.no>
2026-03-10 12:28:50 +0100

    TOB-6035: Test på at validering av summer etter mapping kaster exception ved feil.
    Co-authored-by: Ken Dahle <ken.dahle@nav.no>

--- src/test/kotlin/no/nav/sokos/oppgjorsrapporter/rapport/generator/TrekkKredPdfMapperTest.kt
+++ src/test/kotlin/no/nav/sokos/oppgjorsrapporter/rapport/generator/TrekkKredPdfMapperTest.kt
@@ -1,5 +1,6 @@
 package no.nav.sokos.oppgjorsrapporter.rapport.generator
 
+import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import java.time.LocalDate
@@ -12,18 +13,29 @@ import tools.jackson.module.kotlin.readValue
 
 class TrekkKredPdfMapperTest :
     FunSpec({
+        val onoa = OrganisasjonsNavnOgAdresse(organisasjonsnummer = "123456789", navn = "ONOG ONOG OH NO ONONOGO", adresse = "Onogata 33")
+
         test("Generer trekk-kred payload til pdfgen og valider felter") {
             val bestilling =
                 TrekkKredRapportBestilling.xmlMapper.readValue<TrekkKredRapportBestilling>(
                     TestUtil.readFile("mq/trekk_kred_bestilling_flere_enheter.xml")
                 )
 
-            val onoa =
-                OrganisasjonsNavnOgAdresse(organisasjonsnummer = "123456789", navn = "ONOG ONOG OH NO ONONOGO", adresse = "Onogata 33")
             val payload = bestilling.mapTilTrekkKredRapportPdfPayload(onoa, LocalDate.now())
 
             val jsonPayload = Json.encodeToString(payload)
 
             jsonPayload shouldBe TestUtil.readFile("pdfpayload/trekk_kred_flere_enheter_pdf_payload.json")
+        }
+
+        test("Når validering etter mapping feiler så skal det kastes exception") {
+            val bestilling =
+                TrekkKredRapportBestilling.xmlMapper.readValue<TrekkKredRapportBestilling>(
+                    TestUtil.readFile("mq/trekk_kred_bestilling_flere_enheter_med_feil_summering.xml")
+                )
+
+            shouldThrowWithMessage<Exception>("Validering av pdf payload for bestilling feilet") {
+                bestilling.mapTilTrekkKredRapportPdfPayload(onoa, LocalDate.now())
+            }
         }
     })
