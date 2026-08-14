cbaae4b8f62304c2393a9912870da2731d01776a
Harald Meland <harald.roisland.meland@nav.no>
2026-08-14 14:16:45 +0200

    TOB-6893: Dropp HTTP-appender til fordel for ekstra konfig av TCP-appender
    Har ikke fått noen av HTTP-appenderen til å faktisk logge til Team Logs.  Forsøker derfor heller å gjøre ekstra konfig av TCP-appenderen (som foreslått [i denne Slack-tråden](https://nav-it.slack.com/archives/C05DXMSNSV8/p1786707761909469?thread_ts=1782985002.073549&cid=C05DXMSNSV8)).

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ val kotlinxSerializationVersion = "1.11.0"
 val kotliqueryVersion = "1.9.1"
 val ktorVersion = "3.4.3"
 val logbackVersion = "1.5.37"
-val logbackHttpAppenderVersion = "0.3.6-SNAPSHOT"
 val logstashVersion = "9.0"
 val micrometerVersion = "1.17.0-RC1"
 val mockOAuth2ServerVersion = "4.0.0"
@@ -51,7 +50,6 @@ repositories {
             password = githubPassword
         }
     }
-    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
@@ -100,7 +98,6 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
-    runtimeOnly("com.github.e-switch-Solutions-AG:logback-http-appender:$logbackHttpAppenderVersion")
 
     // Inntil vi får tenkt gjennom om vi vil gjøre serialization etc. annerledes så henter vi inn Team HAG sine utils
     implementation("no.nav.helsearbeidsgiver:altinn-pdp-client:${pdpClientVersion}")
